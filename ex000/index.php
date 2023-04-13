c5af7242d95e5d0c1d0e57ffb7758644828683e6
daophin <129446146+daophin@users.noreply.github.com>
2023-04-13 20:31:04 -0300

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primero Programa</title>
+</head>
+<body>
+    <h1>
+        <?php /* echo é um comando para escrever na tela que veio do Pearl e print é um comando também que serve para mostrar algo na tela que veio da linguagem C */
+            echo "Olá, Mundo \u{1F30E} ";
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição.</p>
+</body>
+</html>
